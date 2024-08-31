b920c6e19f51f7ad8bed7d2d399d872f20235a1c
Chandrappa Kadli <Chandrappa.kadli@ycs.in>
2024-08-31 13:51:34 +0530

    CheckBox

--- src/App.js
+++ src/App.js
@@ -5,100 +5,115 @@ const initialItems = [
   { id: 2, description: "Socks", quantity: 12, packed: false },
 ];
 
-
-
 export default function App() {
-  const[items,setItems]=useState([]);
+  const [items, setItems] = useState([]);
 
-  function handleAddItems(item){
-    setItems((items)=>[...items,item])
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
   }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div>
       <Logo />
-      <Form  onAddItems={handleAddItems}/>
-      <PackingList items={items} />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onToggleItem={handleToggleItem} />
       <Stats />
     </div>
   );
 }
 
 function Logo() {
-  return <h1> Far Away </h1>;
+  return <h1>Far Away</h1>;
 }
 
 function Form({ onAddItems }) {
-  const [description,setDescription]=useState("");
-  const [quantity,setQuantity]=useState(1);
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
-  function handleSubmit(e){
-   e.preventDefault();
-   
-   if(!description)return;
+  function handleSubmit(e) {
+    e.preventDefault();
 
-  const newIteam={description,quantity,packed:false,id:Date.now()};
-  console.log(newIteam);
+    if (!description) return;
 
-  onAddItems(newIteam);
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
 
-  setDescription("");
-  setQuantity(1);
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
-    <form className="add-form"   onSubmit={handleSubmit}>
-      <h3>What you need for your trip ?</h3>
-      <select value={quantity} onChange={(e)=>setQuantity (e.target.value)}>
-      {Array.from({length:20},(_, i)=>i+1).map
-      ((num)=>(
-        <option value={num} key={num}>{num}</option>))}
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What you need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
-      <input type="text" placeholder="Item..." value={description}
-      onChange={(e)=>{
-        setDescription(e.target.value);
-      }}/>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
       <button type="submit">Add</button>
     </form>
   );
 }
 
-function PackingList({items}) {
+function PackingList({ items, onToggleItem }) {
   return (
-  <div className="list">
-  <ul>{items.map((item)=>(
-    <Item item={item} key={item.id}/>
-  ))}</ul>  
-  </div>
-);
-}
-
-
-function Item({item}){
-return (
-<li>
-  <span style={item.packed?{textDecoration:"line-through"}:{}}>
-  {item.description}
-  ({item.quantity})
-  </span>
-
-  <button>&items;</button>
-  
-  </li>
+    <div className="list">
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} onToggleItem={onToggleItem} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
+function Item({ item, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} ({item.quantity})
+      </span>
+      <button>&times;</button>
+    </li>
+  );
+}
 
 function Stats() {
   return (
-    <>
     <footer className="stats">
-    
-      {" "}
-      <div className="stats">
-        <em>You have X items on your list ,and you already packed X(X%)</em>
-      </div>
-      </footer>
-     
-    </>
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
   );
 }
